1aa1b62353a34d75d7e1cd42c91cc1b18d3b81b5
Lana <apple@Apples-MacBook-Air.local>
2024-10-20 16:04:31 +0700

    third commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,8 @@
 import About from "../components/About/About";
-import BmiCalc from "../components/BMI/BmiCalc";
-import Blog from "../components/Blog/Blog";
-import ChooseUs from "../components/ChooseUs/ChooseUs";
-import CtaBanner from "../components/CtaBanner/CtaBanner";
 import Featured from "../components/Featured/Featured";
 import Footer from "../components/Footer/Footer";
 import Gallery from "../components/Gallery/Gallery";
 import Hero from "../components/Hero/Hero";
-import Pricing from "../components/Pricing/Pricing";
-import Testimonials from "../components/Testimonials/Testimonials";
-import Trainers from "../components/Trainers/Trainers";
 
 function Home() {
   return (
